761ece4a51ad2c05bbba06acb9b7a41bca2938dc
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-10-09 23:38:27 +0530

    feat: set staff list as grid view

--- staff/academic.php
+++ staff/academic.php
@@ -5,238 +5,136 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
 	<title>Department of Archaeology</title>
 
 	<?php require_once "../assets/header.php"; ?>
 	
-	<!-- begin body content -->
+	<style>
+		.profile-grid {
+			display: grid;
+			grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+			gap: 20px;
+			margin: 20px 0;
+		}
+		.profile-item {
+			border: 1px solid #ddd;
+			padding: 20px;
+			text-align: center;
+			border-radius: 10px;
+			box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+		}
+		.profile-item img {
+			max-width: 100%;
+			border-radius: 50%;
+		}
+		.profile-item h4 {
+			margin-top: 15px;
+			font-size: 1.2rem;
+		}
+		.profile-item h5 {
+			color: #555;
+			font-size: 1rem;
+		}
+		.profile-item p {
+			margin: 10px 0;
+			font-size: 0.9rem;
+		}
+	</style>
 
+</head>
+<body>
 	<div class="container">
 		<ol class="breadcrumb">
-		  <li><a href="../index.php">Home</a></li>
-		  <li class="active">Staff</li>
-		  <li class="active">Academic</li>
+			<li><a href="../index.php">Home</a></li>
+			<li class="active">Staff</li>
+			<li class="active">Academic</li>
 		</ol>
         <div class="page">
-		<h3>Academic Staff</h3>
-        <hr />
-        
-        <!--<div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/group.JPG" alt="academic" width="960" height="359" ></div></div>-->
-        
-        
-         <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"></div>
-         </div>
-         <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/chandranew.jpg" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="rmm.php">Prof. R.M.M. Chandraratne</a></h4>
-          <h5>Professor, Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-	        	<blockquote>
-        		  <em>BA (Hons) (Perad), MA (Poona), Ph.D. (Pune), FSLCA</em><br />
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:m.chandraratne@arts.pdn.ac.lk">m.chandraratne@arts.pdn.ac.lk</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span>+94 0812392583</span>
-        	  </p>
-	        		        </div>
-		</div>
-        
-        
-         <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/proframbukwella.jpg" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="rabukwella.php">Prof. Chulani Rambukwella</a></h4>
-               <h5>Chair Professor of Archaeology, Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-                
-	        	<blockquote>
-        		  <em>Phd (UK), MA (Peradeniya), BA (Peradeniya)</em><br />
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:chulanir@pdn.ac.lk">chulanir@pdn.ac.lk</a>/<a href="mailto:chulanimw@gmail.com">chulanimw@gmail.com</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span>+94 718600523 , +94 77 3592153</span>
-        	  </p>
-	          </div>
-		</div>
-   
-        
-         <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/ast.png" alt="academic" width="147" height="150" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="ast.php"> Dr. A.S.T. Rajapakse </a></h4>
-               <h5>Senior Lecturer, Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-                
-	        	<blockquote>
-        		  <em>B.A. (Peradeniya),P.G. Dip.(IHS, Netherlands), M.A. (I.P. University), Ph.D. (Peradeniya)</em><br />
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:tsaru2003@yahoo.com">tsaru2003@yahoo.com</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span>+94 71 4395277</span>
-        	  </p>
-	        	    </div>
-		</div>
-       
-      <!-- <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/karunarathna.jpg" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="agdn.php"> Dr. A.G.D.N. Karunarathna </a></h4>
-                
-	        	<blockquote>
-        		  <em>BA (Hons) in Archaeology (Peradeniya), M Phil (Peradeniya), Ph.D. (Newcastle, United Kingdom), PG Certificate (Newcastle, United Kingdom) </em><br />
-					
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:kdulma@yahoo.com">kdulma@yahoo.com</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span>+94 714418676</span>
-        	  </p>
-	         </div>
-		</div>-->
+			<h3>Academic Staff</h3>
+			<hr />
 
-		 <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/sandya.png" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="sandya.php">Ms. K.G.A.M. Sandya Kumari Nawarathna Menike</a></h4>
-               <h5>Lecturer, Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-                
-	        	<blockquote>
-        		  <em>BA(Hons)Peradeniya,MSc(PGIAR),Ph.D.Reading(PGIAR ) MSLCA</em><br />
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:sannawarathna@gmail.com">sannawarathna@gmail.com</a>/<a href="mailto:nawarathnasknm@pdn.ac.lk">nawarathnasknm@pdn.ac.lk</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span>+94 714 823 384</span>
-        	  </p>
-	        	  </div>
+			<div class="profile-grid">
+				<div class="profile-item">
+					<img src="../assets/images/academic/chandranew.jpg" alt="Prof. R.M.M. Chandraratne" width="147" height="182">
+					<h4><a href="rmm.php">Prof. R.M.M. Chandraratne</a></h4>
+					<h5>Professor, Department of Archaeology</h5>
+					<p>BA (Hons) (Perad), MA (Poona), Ph.D. (Pune), FSLCA</p>
+					<p>Email: <a href="mailto:m.chandraratne@arts.pdn.ac.lk">m.chandraratne@arts.pdn.ac.lk</a></p>
+					<p>Phone: +94 0812392583</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/proframbukwella.jpg" alt="Prof. Chulani Rambukwella" width="147" height="182">
+					<h4><a href="rabukwella.php">Prof. Chulani Rambukwella</a></h4>
+					<h5>Chair Professor of Archaeology</h5>
+					<p>Phd (UK), MA (Peradeniya), BA (Peradeniya)</p>
+					<p>Email: <a href="mailto:chulanir@pdn.ac.lk">chulanir@pdn.ac.lk</a>/<a href="mailto:chulanimw@gmail.com">chulanimw@gmail.com</a></p>
+					<p>Phone: +94 718600523</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/ast.png" alt="Dr. A.S.T. Rajapakse" width="147" height="150">
+					<h4><a href="ast.php">Dr. A.S.T. Rajapakse</a></h4>
+					<h5>Senior Lecturer</h5>
+					<p>B.A. (Peradeniya),P.G. Dip.(IHS, Netherlands), M.A. (I.P. University), Ph.D. (Peradeniya)</p>
+					<p>Email: <a href="mailto:tsaru2003@yahoo.com">tsaru2003@yahoo.com</a></p>
+					<p>Phone: +94 71 4395277</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/sandya.png" alt="Ms. K.G.A.M. Sandya Kumari Nawarathna Menike" width="147" height="182">
+					<h4><a href="sandya.php">Ms. K.G.A.M. Sandya Kumari Nawarathna Menike</a></h4>
+					<h5>Lecturer</h5>
+					<p>BA(Hons)Peradeniya,MSc(PGIAR),Ph.D.Reading(PGIAR) MSLCA</p>
+					<p>Email: <a href="mailto:sannawarathna@gmail.com">sannawarathna@gmail.com</a>/<a href="mailto:nawarathnasknm@pdn.ac.lk">nawarathnasknm@pdn.ac.lk</a></p>
+					<p>Phone: +94 714 823 384</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/uditha.jpg" alt="Dr. Uditha Jinadasa" width="147" height="182">
+					<h4><a href="uditha.php">Dr. Uditha Jinadasa</a></h4>
+					<h5>Lecturer</h5>
+					<p>BA & MSc in Geo-Informatics (Peradeniya), PhD (Leiden)</p>
+					<p>Email: <a href="mailto:u.n.jinadasa@gmail.com">u.n.jinadasa@gmail.com</a>/<a href="mailto:ujinadasa@pdn.ac.lk">ujinadasa@pdn.ac.lk</a></p>
+					<p>Phone: +94 71 497 0718</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/piyumi.png" alt="Ms. Piyumi Embuldeniya" width="147" height="182">
+					<h4><a href="piyumi.php">Ms. Piyumi Embuldeniya</a></h4>
+					<h5>Lecturer (Probationary)</h5>
+					<p>MSSc. (Kelaniya), PGDip. in Museology (PGIAR, Kelaniya), B.A. (Kelaniya)</p>
+					<p>Email: <a href="mailto:piyumie@arts.pdn.ac.lk">piyumie@arts.pdn.ac.lk</a></p>
+					<p>Phone: +94 772383788</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/buddi.jpg" alt="Mr. Buddhisha Weerasuriya" width="147" height="182">
+					<h4><a href="#">Mr. Buddhisha Weerasuriya</a></h4>
+					<h5>Lecturer (Probationary)</h5>
+					<p>M.Phil (Peradeniya) - Reading, B.A (Hons) (Peradeniya)</p>
+					<p>Email: <a href="mailto:buddisha@arts.pdn.ac.lk">buddisha@arts.pdn.ac.lk</a></p>
+					<p>Phone: +94 71 5206865</p>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/senarat.jpg" alt="Prof. Senarat Paranavitana" width="172" height="182">
+					<h4><a href="#">Prof. Senarat Paranavitana</a></h4>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/UnknownProfile.png" alt="Prof. P.L. Prematilleke" width="172" height="182">
+					<h4><a href="#">Prof. P.L. Prematilleke</a></h4>
+				</div>
+
+				<div class="profile-item">
+					<img src="../assets/images/academic/seneviratna.jpg" alt="Prof. S.D.S. Seneviratne" width="172" height="182">
+					<h4><a href="#">Prof. S.D.S. Seneviratne</a></h4>
+				</div>
+			</div>
 		</div>
-
-
-		 <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/uditha.jpg" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="uditha.php">Dr. Uditha Jinadasa</a></h4>
-	        	<h5>Lecturer, Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-
-                
-	        	<blockquote>
-        		  <em>BA & MSc in Geo-Informatics (Peradeniya), PhD (Leiden)</em><br />
-					
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:u.n.jinadasa@gmail.com">u.n.jinadasa@gmail.com</a>/<a href="mailto:ujinadasa@pdn.ac.lk">ujinadasa@pdn.ac.lk</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span>+94 71 497 0718</span>
-        	  </p>
-	        	      </div>
-		</div>
-
-		 <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/piyumi.png" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="piyumi.php">Ms. Piyumi Embuldeniya</a></h4>
-                <h5>Lecturer (Probationary), Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-
-	        	<blockquote>
-        		  <em>MSSc. (Kelaniya), PGDip. in Museology (PGIAR, Kelaniya), B.A. (Kelaniya)</em><br />
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:piyumie@arts.pdn.ac.lk">piyumie@arts.pdn.ac.lk</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span> +94 772383788, +94715649994</span>
-        	  </p>
-	        	  </div>
-		</div>
-        
-        <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/buddi.jpg" alt="academic" width="147" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="#">Mr.Buddhisha Weerasuriya</a></h4>
-                <h5>Lecturer (Probationary), Department of Archaeology, Faculty of Arts, University of Peradeniya, Sri Lanka.</h5>
-
-	        	<blockquote>
-        		  <em>M.phil (Peradeniya) - Reading, B.A (Hons) (Peradeniya)</em><br />
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:piyumie@pdn.ac.lk">buddisha@arts.pdn.ac.lk</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<span> +94 71 5206865</span>
-        	  </p>
-	        	  </div>
-		</div>
-
-
-
-        
-        <h4>Retired Professor of Archaeology </h4>
-        <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/senarat.jpg" alt="academic" width="172" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="#"> Prof. Senarat Paranavitana</a></h4>
-                <!--<h5>Emeritus professor</h5>-->
-	        	<blockquote>
-        		  <!--<em>B.A. (Delhi, India), M.A. & Ph.D. (JNU, India)</em><br />
-					<!--<span>Sociology of Religion and Cultural Anthropological Issues in South Asia</span>-->
-	        	</blockquote>
-        	  <p>
-	        	<!--<span class="fa fa-envelope-o"></span>
-	        	<!--<span><a href="mailto:susenevi@hotmail.com">susenevi@hotmail.com</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<!--<span>+94 71 4395277</span>-->
-        	  </p>
-	        	<!--<a class="btn btn-default" href="Hennayake.php" role="button">View Profile</a>-->	        </div>
-		</div>
-        <h4>Retired Professor</h4>
-        <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/UnknownProfile.png" alt="academic" width="172" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="#"> Prof. P.L. Prematilleke</a></h4>
-                <!--<h5>Emeritus professor</h5>-->
-	        	<blockquote>
-        		  <!--<em>B.A. (Delhi, India), M.A. & Ph.D. (JNU, India)</em><br />
-					<!--<span>Sociology of Religion and Cultural Anthropological Issues in South Asia</span>-->
-	        	</blockquote>
-        	  <p>
-	        	<!--<span class="fa fa-envelope-o"></span>
-	        	<!--<span><a href="mailto:susenevi@hotmail.com">susenevi@hotmail.com</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<!--<span>+94 71 4395277</span>-->
-        	  </p>
-	        	<!--<a class="btn btn-default" href="Hennayake.php" role="button">View Profile</a>-->	        </div>
-		</div>
-        <h4>Professor Emeritus </h4>
-        <div class="row profileBar">
-	        <div class="col-md-offset-1 col-md-3 face"><img src="../assets/images/academic/seneviratna.jpg" alt="academic" width="172" height="182" class="img-responsive"></div>
-	        <div class="col-md-7 description">
-	        	<h4><a href="#"> Prof. S.D.S. Seneviratne</a></h4>
-                <!--<h5>Emeritus professor</h5>-->
-	        	<blockquote>
-        		  <em>B.A. (Delhi, India), M.A. & Ph.D. (JNU, India)</em><br />
-					<!--<span>Sociology of Religion and Cultural Anthropological Issues in South Asia</span>-->
-	        	</blockquote>
-        	  <p>
-	        	<span class="fa fa-envelope-o"></span>
-	        	<span><a href="mailto:susenevi@hotmail.com">susenevi@hotmail.com</a></span><br />
-	        	<span class="fa fa-phone"></span>
-	        	<!--<span>+94 71 4395277</span>-->
-        	  </p>
-	        	<!--<a class="btn btn-default" href="Hennayake.php" role="button">View Profile</a>-->	        </div>
-		</div>
-        </div>
 	</div>
 
 	<?php require_once "../assets/footer.php" ?>
