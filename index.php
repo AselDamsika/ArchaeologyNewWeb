a3aff9be0e76bb2c1ee43b4272e372b81e8b5ddb
vimansha <vimanshamadushani88@gmail.com>
2024-10-29 23:19:43 +0530

    feat: image slider

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-	$pageId = "home";
+$pageId = "home";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	
+
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
 	<title>Department of Archaeology</title>
@@ -13,80 +14,105 @@
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
 
 	<?php require_once "assets/header.php"; ?>
-	
-	<!-- begin body content -->
 
-	<!-- Start WOWSlider.com BODY section -->
-	<div class="slider">
-		<div id="wowslider-container1">
-			<div class="ws_images">
-				<ul>
-                	<li><img src="assets/data1/images/07-08-2024.jpg" alt="Department of Archaeology" title="Department of Archaeology" id="wows1_0"/></li>
-					<li><img src="assets/data1/images/001.jpg" alt="Department of Archaeology" title="Department of Archaeology" id="wows1_0"/></li>
-					<li><img src="assets/data1/images/005new.jpg" alt="" title="" id="wows1_1"/></li>
-					<li><img src="assets/data1/images/008new.jpg" alt="" title="" id="wows1_2"/></li>
-					<li><img src="assets/data1/images/002new.jpg" alt="slider" title="" id="wows1_3"/></li>
-					<li><img src="assets/data1/images/006new.jpg" alt="" title="" id="wows1_4"/></li>
-				</ul>
-			</div>
-			<div class="ws_bullets">
-				<div>
-					<a href="#" title="Department of Archaeology"><span><img src="assets/data1/tooltips/001new2.jpg" alt="Department of Archaeology"/>1</span></a>
-					<a href="#" title=""><span><img src="assets/data1/tooltips/005new.jpg" alt=""/>2</span></a>
-					<a href="#" title=""><span><img src="assets/data1/tooltips/007new.jpg" alt=""/>3</span></a>
-					<a href="#" title=""><span><img src="assets/data1/tooltips/002new.jpg" alt=""/>4</span></a>
-					<a href="#" title=""><span><img src="assets/data1/tooltips/006new.jpg" alt=""/>5</span></a>
+	<!-- Include Bootstrap CSS -->
+
+	<div id="mySlider" class="carousel slide" data-ride="carousel" data-interval="3000">
+		<!-- Indicators -->
+		<ul class="carousel-indicators">
+			<li data-target="#mySlider" data-slide-to="0" class="active"></li>
+			<li data-target="#mySlider" data-slide-to="1"></li>
+			<li data-target="#mySlider" data-slide-to="2"></li>
+			<li data-target="#mySlider" data-slide-to="3"></li>
+			<li data-target="#mySlider" data-slide-to="4"></li>
+		</ul>
+
+		<!-- The slideshow -->
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="assets/data1/images/001new.jpg" alt="Department of Archaeology" class="d-block w-100">
+				<div class="carousel-caption">
+					<h3>Department of Archaeology</h3>
 				</div>
 			</div>
-		<div class="ws_shadow"></div>
-		</div>	
-		<!-- End WOWSlider.com BODY section -->
+			<div class="carousel-item">
+				<img src="assets/data1/images/002new.jpg" alt="Department of Archaeology" class="d-block w-100">
+				<div class="carousel-caption">
+					<h3>Department of Archaeology</h3>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img src="assets/data1/images/005new.jpg" alt="" class="d-block w-100">
+			</div>
+			<div class="carousel-item">
+				<img src="assets/data1/images/008new.jpg" alt="" class="d-block w-100">
+			</div>
+			<div class="carousel-item">
+				<img src="assets/data1/images/002new.jpg" alt="slider" class="d-block w-100">
+			</div>
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="carousel-control-prev" href="#mySlider" data-slide="prev">
+			<span class="carousel-control-prev-icon"></span>
+		</a>
+		<a class="carousel-control-next" href="#mySlider" data-slide="next">
+			<span class="carousel-control-next-icon"></span>
+		</a>
 	</div>
 
+	<!-- Include Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
 	<div class="frontHolder">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 welcomeNote">
-                    <h3><i class="fa fa-quote-left"></i> Welcome to the Department of Archaeology <i class="fa fa-quote-right"></i></h3>
-                    <hr />
-                    <blockquote>
-                        <p><span class="highlight">The Department of Archaeology, University of Peradeniya</span> was established as a separate Department in 1976, is one of the largest Departments in the university system of Sri Lanka. At present, over one hundred undergraduates are following the Special and General Degree courses in Sinhala and English media at the Department.
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 welcomeNote">
+					<h3><i class="fa fa-quote-left"></i> Welcome to the Department of Archaeology <i class="fa fa-quote-right"></i></h3>
+					<hr />
+					<blockquote>
+						<p><span class="highlight">The Department of Archaeology, University of Peradeniya</span> was established as a separate Department in 1976, is one of the largest Departments in the university system of Sri Lanka. At present, over one hundred undergraduates are following the Special and General Degree courses in Sinhala and English media at the Department.
 
-The teaching of archaeology at Peradeniya, was originally introduced as an auxiliary subject to history, as a sub-department, for the purpose of training future archaeological officers required by the state. In 1957, Dr. Senarat Paranavitana on his retirement from the post of Commissioner of Archaeology was appointed as the Professor of Archaeology, which marked the official beginning of the Department of Archaeology, University of Peradeniya. The notable archaeologists, namely, Prof Senarat Paranavitana, Prof. P.L. Prematilleke and Prof. Sudharshan Seneviratne have served in the Department.
-</p>
-                        
-                        <hr />
+							The teaching of archaeology at Peradeniya, was originally introduced as an auxiliary subject to history, as a sub-department, for the purpose of training future archaeological officers required by the state. In 1957, Dr. Senarat Paranavitana on his retirement from the post of Commissioner of Archaeology was appointed as the Professor of Archaeology, which marked the official beginning of the Department of Archaeology, University of Peradeniya. The notable archaeologists, namely, Prof Senarat Paranavitana, Prof. P.L. Prematilleke and Prof. Sudharshan Seneviratne have served in the Department.
+						</p>
 
-                        <!--<p>Among the pioneering sociologists associated with the Department were the <span class="highlight"><em>Prof.Bryce Ryan, Prof.Ralph Pieris, Prof.S.J. Tambiah, Prof. Gananath Obeyesekere, Prof.Laksiri Jayasuriya, Prof. Nur Yalman, Prof. E.R. Leach, Prof. H.L.Seneviratne and Prof. Kitsiri Malalgoda. Prof. K. Tudor Silva, Emeritus Prof. W. M. Sirisena</em> and <em>Prof. M. W. Amarasiri De Silva</em></span> another prominent members who have served in the Department in the recent past.</p>-->
-                    </blockquote>  
-                </div>
+						<hr />
 
-                <div class="col-md-4 noticeBoard">
-                    <h3>News &amp; Events</h3>
-                    <hr />
-                    
-                    
-                    <h4><i class="fa fa-tags"></i>Old Money and Ancient Writing:  Archaeology undergraduates visit the National Museum and (state) Department of Archaeology to study coins and inscriptions</h4> <h5><a href="videos/Study tour.pdf" target="_blank">More Info </a></h5>
-                    <video width="320" height="240" controls>
-  					<source src="videos/archaeology.mov" type="video/mp4">
-  					</video>
-                    <hr />
-                    
-                    
-                    <h4><i class="fa fa-tags"></i>Maritime Archaeology Training Programme at the Maritime Archaeology Unit of the Central Cultural Fund in Galle.</h4>
-                    <video width="320" height="240" controls>
-  					<source src="videos/Department of Archaeology MAU Training,Galle.mp4" type="video/mp4">
-  					</video>
-                    <hr />
-                    
-                    <h4> <i class="fa fa-tags"></i> Rambewa Kadahatha Aloka Pabbatha Vihara Excavation conducted by the Department of Archaeology and Heritage Management, Rajarata University of Sri Lanka. </h4>
-                    <video width="320" height="240" controls>
-  					<source src="videos/Department of Archaeology Rambewa Excavation.mp4" type="video/mp4">
-  					</video>
-                    <hr />
-                    
-                    
-                    <!--<h4><i class="fa fa-tags"></i>International Conference on Social Work - Social Development and Sustainable Development Goals</h4>
+						<!--<p>Among the pioneering sociologists associated with the Department were the <span class="highlight"><em>Prof.Bryce Ryan, Prof.Ralph Pieris, Prof.S.J. Tambiah, Prof. Gananath Obeyesekere, Prof.Laksiri Jayasuriya, Prof. Nur Yalman, Prof. E.R. Leach, Prof. H.L.Seneviratne and Prof. Kitsiri Malalgoda. Prof. K. Tudor Silva, Emeritus Prof. W. M. Sirisena</em> and <em>Prof. M. W. Amarasiri De Silva</em></span> another prominent members who have served in the Department in the recent past.</p>-->
+					</blockquote>
+				</div>
+
+				<div class="col-md-4 noticeBoard">
+					<h3>News &amp; Events</h3>
+					<hr />
+
+
+					<h4><i class="fa fa-tags"></i>Old Money and Ancient Writing: Archaeology undergraduates visit the National Museum and (state) Department of Archaeology to study coins and inscriptions</h4>
+					<h5><a href="videos/Study tour.pdf" target="_blank">More Info </a></h5>
+					<video width="320" height="240" controls>
+						<source src="videos/archaeology.mov" type="video/mp4">
+					</video>
+					<hr />
+
+
+					<h4><i class="fa fa-tags"></i>Maritime Archaeology Training Programme at the Maritime Archaeology Unit of the Central Cultural Fund in Galle.</h4>
+					<video width="320" height="240" controls>
+						<source src="videos/Department of Archaeology MAU Training,Galle.mp4" type="video/mp4">
+					</video>
+					<hr />
+
+					<h4> <i class="fa fa-tags"></i> Rambewa Kadahatha Aloka Pabbatha Vihara Excavation conducted by the Department of Archaeology and Heritage Management, Rajarata University of Sri Lanka. </h4>
+					<video width="320" height="240" controls>
+						<source src="videos/Department of Archaeology Rambewa Excavation.mp4" type="video/mp4">
+					</video>
+					<hr />
+
+
+					<!--<h4><i class="fa fa-tags"></i>International Conference on Social Work - Social Development and Sustainable Development Goals</h4>
                     <ul>
                     <li>International Conference on <strong>Social Work - Social Development and Sustainable Development Goals,</strong> will be held on 29<sup>th</sup> - 30<sup>th</sup> September 2016. The conference organized by International Consortium for Social Development, Asia Pacific Branch with the Department of Sociology, University of Peradeniya, Kandy, Sri Lanka & Charles Sturt University, Australia.</li>
                     <a href="events/conference.php">Learn More</a>
@@ -97,13 +123,14 @@ The teaching of archaeology at Peradeniya, was originally introduced as an auxil
                     <ul>
                     <li>The Sociology day organized by the Department of Sociology held on 8<sup>th</sup> of April, 2015 at the Department of Sociology, University of Peradeniya. Keynote speech by Prof. Gananath Obeysekara</li>
                     <a href="events/socday.php">Learn More</a>    
-                    </ul>-->   
+                    </ul>-->
 
-                </div>
-            </div>
-        </div>
-    </div>    
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<?php require_once "assets/footer.php" ?>
-</body>
+	</body>
+
 </html>
