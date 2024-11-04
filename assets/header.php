a8717fbd5d13622e20a469de74872c530853dd2d
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-11-04 22:57:05 +0530

    comment the button in header

--- assets/header.php
+++ assets/header.php
@@ -153,9 +153,9 @@ if (!isset($pageId)) {
 		<div class="container" style="padding: 0;">
 
 			<div class="navbar-header">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
-				</button>
+				</button> -->
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
