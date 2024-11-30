07c3bcaa4e3b2992de38dcb4a6ad0f2e4338ff39
AselDamiska <162301283+AselDamiska@users.noreply.github.com>
2024-11-30 12:41:17 +0530

    feat: add dummy content to society page

--- assets/header.php
+++ assets/header.php
@@ -235,15 +235,12 @@ if (!isset($pageId)) {
 						</ul>
 					</li>
 
-<<<<<<< HEAD
 					<li class="<?php if ($pageId == 'society') {
 									echo "nav-item active";
 								} ?>"><a href="<?php echo $root; ?>/ac/society.php">Society</a></li>
-=======
 					<li class="<?php if ($pageId == 'about') {
 									echo "nav-item active";
-								} ?>"><a href="<?php echo $root; ?>/about.php">Society</a></li>
->>>>>>> d223f05 (feat: create society tab in navbar)
+								} ?>"><a href="<?php echo $root; ?>/about.php">About</a></li>
 
 					<li class="<?php if ($pageId == 'res') {
 									echo "nav-item active";
