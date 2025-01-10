6c5fb7a9a6154f9286a3582f163dd281a2d39ff5
vimanshaMJ <vimanshamadushani88@gmail.com>
2025-01-10 15:57:47 +0530

    fix

--- staff/academic.php
+++ staff/academic.php
@@ -14,11 +14,6 @@ $pageId = "staff";
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-
 	<?php require_once "../assets/header.php"; ?>
 
 	<!-- Custom CSS for Profile Items -->
@@ -113,7 +108,7 @@ $pageId = "staff";
 		[type=button]:not(:disabled),
 		[type=reset]:not(:disabled),
 		[type=submit]:not(:disabled),
-		button:not(:disabled) {
+		.nav-item button:not(:disabled) {
 			margin: 0 10px;
 			padding: 10px;
 			font-size: 2rem;
