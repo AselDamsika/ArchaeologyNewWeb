fb3b83aaeed6161b6d3d887efe91d3a08a310e49
vimanshaMJ <vimanshamadushani88@gmail.com>
2025-01-12 13:52:51 +0530

    style: contact page

--- contact.php
+++ contact.php
@@ -6,55 +6,83 @@ $pageId = "cnt";
 <html lang="en">
 
 <head>
-
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
 	<title>Department of Archaeology</title>
 
-
 	<style>
-   
 		.contact-container {
-			margin-top: 20px;
 			margin-bottom: 50px;
-		}	
-
-		.contact-info {
-			margin-bottom: 20px;
-			
 		}
 
+		.contact-us {
+			font-size: 4rem;
+			font-weight: 400;
+			color: #003269;
+			font-family: "roboto condensed", sans-serif;
+			padding-left: 0 !important;
+			padding-right: 0 !important;
+		}
+
+		.contact-hr {
+			border: solid 2px #007bff;
+		}
+
+		.contact-row{
+			display: flex;
+			align-items: center;
+			text-align: center;
+		}
+
+		.contact-box {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			background-color: #F0F8FF;
+			margin: 5% 0;
+			padding: 20px;
+			box-shadow: 0 8px 8px rgba(0, 0, 0, 0.1);
+			height: 250px;
+		}
 
 		.contact-title {
-			font-size: 1.5em;
-			font-weight: bold;
-			margin-bottom: 10px;
+			font-size: 3rem;
+			color: #003269;
+			font-weight: 600;
+			text-align: center;
+			margin: 20px 0;
 		}
 
-		.contact-detail {
-			font-size: 1.2em;
-			line-height: 1.5;
+		.contact-box span {
+			color: #1b98e5;
+			font-weight: 500;
+			text-align: left;
 		}
 
-		.span-title {
-			font-weight: bold;
+		.contact-box p {
+			color: #505050 !important;
+			font-size: 1.7rem;
+			font-weight: 400;
+			text-align: left;
 		}
-   
-		.contact-detail a {
-			color: #000;
+		.contact-box a {
+			color: #505050 !important;
+			text-align: left;
+		}
+		.contact-box a:hover {
+			color:rgb(133, 133, 133) !important;
 		}
 
-		.contact-detail a:hover {
-			color: #337ab7;
+		.contact-img {
+			width: 50px;
+			height: 50px;
 		}
 
-    
-  </style>
+	</style>
 </head>
 
 <body>
-
-
 
 	<?php require_once "assets/header.php"; ?>
 
@@ -64,37 +92,55 @@ $pageId = "cnt";
 			<li class="active">Contacts</li>
 		</ol>
 
-		<div class="row">
-			<h2>Contact Us</h2>
+		<div>
+			<h2 class="contact-us">Contact Us</h2>
+			<hr class="contact-hr" />
 
-			<div class="col-md-4 contact-info">
-				<h3 class="contact-title">Postal Address</h3>
-				<p class="contact-detail">
-					Department of Archaeology<br>
-					Faculty of Arts<br>
-					University of Peradeniya<br>
-					Peradeniya 20400<br>
-					Sri Lanka
-				</p>
-			</div>
 
-			<div class="col-md-4 contact-info">
-				<h3 class="contact-title">Telephone</h3>
-				<p class="contact-detail"><span class="span-title">Office : </span>+94 81 239 2553</p>
-				<p class="contact-detail"><span class="span-title">Staff Room : </span>+94 81 239 2555</p>
-				<p class="contact-detail"><span class="span-title">Head of the Department: </span>+94 81 239 2551</p>
-				<p class="contact-detail"><span class="span-title">Laboratory: </span>+94 81 239 2581</p>
-			</div>
+			<div class="row contact-row">
+				<div class="col-md-4" >
+					<div class="contact-box">
+						<img src="assets/data1/images/location.png" alt="" class="contact-img">
+						<h3 class="contact-title">Postal Address</h3>
+						<p>
+							Department of Archaeology<br>
+							Faculty of Arts<br>
+							University of Peradeniya<br>
+							Peradeniya 20400<br>
+							Sri Lanka
+						</p>
+					</div>
 
-			<div class="col-md-4 contact-info">
-				<h3 class="contact-title">Online</h3>
-				
+				</div>
 
-				<p  class="contact-detail"><span class="span-title">Email : </span><a href="mailto:arch.dip.peradeniya@gmail.com">arch.dip.peradeniya@gmail.com</a></p>
-				<p class="contact-detail"><span class="span-title">Web : </span><a href="https://arts.pdn.ac.lk/archaeologynew/" target="_blank">arts.pdn.ac.lk/archaeologynew</a></p>
-				<p class="contact-detail"><span class="span-title">Facebook : </span><a href="https://www.facebook.com/profile.php?id=100086379914591" target="_blank">Facebook Page</a></p>
+				<div class="col-md-4" >
+					<div class="contact-box">
+						<img src="assets/data1/images/telephone.png" alt="" class="contact-img">
+						<h3 class="contact-title">Telephone</h3>
+						<p>
+							<span class="span-title">Office  :  </span>+94 81 239 2553</br>
+							<span class="span-title">Staff Room  :  </span>+94 81 239 2555</br>
+							<span class="span-title">Head  :  </span>+94 81 239 2551</br>
+							<span class="span-title">Laboratory  :  </span>+94 81 239 2581</br>
+						</p>
+					</div>
+
+				</div>
+
+				<div class="col-md-4" >
+					<div class="contact-box">
+						<img src="assets/data1/images/web.png" alt="" class="contact-img">
+						<h3 class="contact-title">Online</h3>
+						<p>
+							<span class="span-title">Email : </span><a href="mailto:arch.dip.peradeniya@gmail.com">arch.dip.peradeniya@gmail.com</a></br>
+							<span class="span-title">Web : </span><a href="https://arts.pdn.ac.lk/archaeologynew/" target="_blank">arts.pdn.ac.lk/archaeologynew</a></br>
+							<span class="span-title">Facebook : </span><a href="https://www.facebook.com/profile.php?id=100086379914591" target="_blank">Facebook Page</a></br>
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
+
 	</div>
 
 	<?php require_once "assets/footer.php" ?>
