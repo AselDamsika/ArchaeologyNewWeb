8100eef041c51d32d5c013f263377997d3d3c658
vimanshaMJ <vimanshamadushani88@gmail.com>
2025-01-12 14:29:24 +0530

    fix

--- assets/footer.php
+++ assets/footer.php
@@ -11,6 +11,7 @@
 			padding: 2% 0;
 		}
 
+<<<<<<< HEAD
 		.fotStripe {
 			padding: 1% 0;
 			color: rgb(179, 179, 179);
@@ -24,6 +25,24 @@
 			align-items: flex-start;
 			padding: 0;
 		}
+=======
+	.fotStripe {
+		padding: 1% 0;
+	}
+	.footer-main-col{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: flex-start;
+		padding: 0;
+	}
+	.footer-col {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 0;
+	}
+>>>>>>> 5ccb247 (fix)
 
 		.footer-col {
 			display: flex;
@@ -38,6 +57,7 @@
 			line-height: 2;
 		}
 
+<<<<<<< HEAD
 		.footer-col a:hover {
 			color: #007bff;
 		}
@@ -76,6 +96,29 @@
 						<a href="https://www.pdn.ac.lk/login/" target="_blank">LOGIN</a>
 						<a href="<?php echo $root; ?>/contact.php">CONTACTS</a>
 					</div>
+=======
+<footer>
+	<div class="fotBar">
+		<div class="container">
+			<div class="row footer-row">
+				<div class="col-md-3 footer-main-col">
+					<h3 class="quick-links">Quick Links</h3>
+				</div>
+				<div class="col-md-3 footer-col">
+					<a href="<?php echo $root ; ?>/ac/under.php">UNDERGRADUATE</a>
+					<a href="http://www.pgihs.lk/">POSTGRADUATE</a>
+					<a href="#">ALUMNI</a>
+				</div>
+				<div class="col-md-3 footer-col">
+					<a href="<?php echo $root ; ?>/">ARCHAEOLOGY</a>
+					<a href="http://arts.pdn.ac.lk/" target="_blank">ARTS</a>
+					<a href="http://www.pdn.ac.lk/" target="_blank">UoP</a>
+				</div>
+				<div class="col-md-3 footer-col">
+					<a href="https://webmail.pdn.ac.lk/" target="_blank">WEBMAIL</a>
+					<a href="https://www.pdn.ac.lk/login/" target="_blank">LOGIN</a>
+					<a href="<?php echo $root ; ?>/contact.php">CONTACTS</a>
+>>>>>>> 5ccb247 (fix)
 				</div>
 			</div>
 		</div>
