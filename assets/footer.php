0d819a53148198f10d600c1b8798d67b9b5b7657
vimanshaMJ <vimanshamadushani88@gmail.com>
2025-01-12 17:04:19 +0530

    fix

--- assets/footer.php
+++ assets/footer.php
@@ -11,9 +11,9 @@
 			padding: 2% 0;
 		}
 
-<<<<<<< HEAD
 		.fotStripe {
-			padding: 1% 0;
+			margin-top: 5%;
+			padding: 0% 0;
 			color: rgb(179, 179, 179);
 			font-size: 1.5rem;
 		}
@@ -25,24 +25,21 @@
 			align-items: flex-start;
 			padding: 0;
 		}
-=======
-	.fotStripe {
-		padding: 1% 0;
-	}
-	.footer-main-col{
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		align-items: flex-start;
-		padding: 0;
-	}
-	.footer-col {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		padding: 0;
-	}
->>>>>>> 5ccb247 (fix)
+
+		.footer-main-col {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			align-items: flex-start;
+			padding: 0;
+		}
+
+		.footer-col {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			padding: 0;
+		}
 
 		.footer-col {
 			display: flex;
@@ -57,7 +54,6 @@
 			line-height: 2;
 		}
 
-<<<<<<< HEAD
 		.footer-col a:hover {
 			color: #007bff;
 		}
@@ -70,14 +66,10 @@
 </head>
 
 <body>
-
-
-
-
 	<footer>
 		<div class="fotBar">
-			<div class="container-fluid"> <!-- Changed from container to container-fluid -->
-				<div class="row">
+			<div class="container">
+				<div class="row footer-row">
 					<div class="col-md-3 footer-main-col">
 						<h3 class="quick-links">Quick Links</h3>
 					</div>
@@ -96,33 +88,9 @@
 						<a href="https://www.pdn.ac.lk/login/" target="_blank">LOGIN</a>
 						<a href="<?php echo $root; ?>/contact.php">CONTACTS</a>
 					</div>
-=======
-<footer>
-	<div class="fotBar">
-		<div class="container">
-			<div class="row footer-row">
-				<div class="col-md-3 footer-main-col">
-					<h3 class="quick-links">Quick Links</h3>
-				</div>
-				<div class="col-md-3 footer-col">
-					<a href="<?php echo $root ; ?>/ac/under.php">UNDERGRADUATE</a>
-					<a href="http://www.pgihs.lk/">POSTGRADUATE</a>
-					<a href="#">ALUMNI</a>
-				</div>
-				<div class="col-md-3 footer-col">
-					<a href="<?php echo $root ; ?>/">ARCHAEOLOGY</a>
-					<a href="http://arts.pdn.ac.lk/" target="_blank">ARTS</a>
-					<a href="http://www.pdn.ac.lk/" target="_blank">UoP</a>
-				</div>
-				<div class="col-md-3 footer-col">
-					<a href="https://webmail.pdn.ac.lk/" target="_blank">WEBMAIL</a>
-					<a href="https://www.pdn.ac.lk/login/" target="_blank">LOGIN</a>
-					<a href="<?php echo $root ; ?>/contact.php">CONTACTS</a>
->>>>>>> 5ccb247 (fix)
 				</div>
 			</div>
-		</div>
-		<div class="fotStripe text-center">Copyright ©<span id="year"></span> All Rights Reserved - Department of Archaeology - University of Peradeniya</div>
+			<div class="fotStripe text-center">Copyright ©<span id="year"></span> All Rights Reserved - Department of Archaeology - University of Peradeniya</div>
 	</footer>
 
 	<script src="<?php echo $root; ?>/assets/js/jquery.js"></script>
